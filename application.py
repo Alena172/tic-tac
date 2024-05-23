5197c954ced82f09bc2a4453979fafa4353b33a9
Alena172 <149298093+Alena172@users.noreply.github.com>
2024-05-23 16:46:06 +0300

    Update application.py

--- application.py
+++ application.py
@@ -6,7 +6,7 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'secret!'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/tic'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://tic_user:Bgzcm3mEzunzP8K227xKqcY6tCiXLbIk@dpg-cp7kcnmd3nmc73btcp90-a.oregon-postgres.render.com/tic'
 
     db.init_app(app)
 
